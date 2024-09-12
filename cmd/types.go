4feeb2e1abca919df590b53405a11598ad996aba
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-09-12 23:40:28 +0100

    Add status to response

--- cmd/types.go
+++ cmd/types.go
@@ -1,11 +1,13 @@
 package main
 
 type FileTestsResponse struct {
+	Status     bool
 	Dayname    string
 	Exercises  []string
 	FilesTests map[string]interface{}
 }
 
 type FileTestsResponseError struct {
+	Status  bool
 	Message string
 }
